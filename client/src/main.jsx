36b7e8df55b644a68187393d8de70e9cdca019db
ManashSwain <manash0802@gmail.com>
2025-03-15 14:30:04 +0530

    added browser router and formatted main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import AppContext from './context/AppContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AppContext from "./context/AppContext.jsx";
+import BrowserRouter from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <AppContext>
-    <App />
+      <StrictMode>
+        <App />
+      </StrictMode>
     </AppContext>
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
