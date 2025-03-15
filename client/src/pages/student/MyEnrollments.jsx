b9077ee9cffc9540de5e650a652efe020b17892e
ManashSwain <manash0802@gmail.com>
2025-03-15 13:53:05 +0530

    added page structure for student

---
+++ client/src/pages/student/MyEnrollments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyEnrollments = () => {
+  return (
+   <>
+   <h1>My enrollments</h1>
+   </>
+  )
+}
+
+export default MyEnrollments
---
+++ client/src/pages/student/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Player = () => {
+  return (
+   <>
+   <h1>Player</h1>
+   </>
+  )
+}
+
+export default Player
