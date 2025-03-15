181c3b0952afa03161c6a4b9d249afd9db44c0a4
ManashSwain <manash0802@gmail.com>
2025-03-15 16:01:26 +0530

    implemented route structure

---
+++ client/src/pages/educator/Educator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Educator = () => {
+  return (
+    <>
+    <h1>Educator</h1>
+    </>
+  )
+}
+
+export default Educator
--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const CourseList = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <h1>Course list</h1>
+    </>
   )
 }
 
