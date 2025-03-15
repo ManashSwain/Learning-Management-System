6d32e41ec9f77f9322c6407d6643d9cb043dc14e
ManashSwain <manash0802@gmail.com>
2025-03-15 13:58:34 +0530

    added page structure of educator

---
+++ client/src/pages/educator/MyCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyCourses = () => {
+  return (
+   <>
+   <h1>My Courses</h1>
+   </>
+  )
+}
+
+export default MyCourses
---
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentsEnrolled = () => {
+  return (
+    <>
+    <h1>Students enrolled</h1>
+    </>
+  )
+}
+
+export default StudentsEnrolled
