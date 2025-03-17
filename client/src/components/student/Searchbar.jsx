7323fcec00b8b19d28a914630edefbf895f980cc
ManashSwain <manash0802@gmail.com>
2025-03-17 14:07:33 +0530

    implemented search functionality

--- client/src/components/student/Searchbar.jsx
+++ client/src/components/student/Searchbar.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Searchbar = () => {
+
+  const [input , setInput] = useState('');
+  const navigate = useNavigate();
+
+  const searchCourses = (e)=>{
+    e.preventDefault();
+    navigate(`/course-list/` + input);
+
+  }
   return (
     <>
     
@@ -12,8 +22,8 @@ const Searchbar = () => {
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search for courses..." required />
-        <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
+        <input type="search" value={input} onChange={(e)=>{setInput(e.target.value)}} id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search for courses..." required />
+        <button type="submit" onClick={searchCourses} className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
     </div>
 </form>
 
