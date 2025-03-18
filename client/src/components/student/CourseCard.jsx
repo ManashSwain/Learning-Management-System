d736af80fd09c6cbc9ca5c62c6ab9e401508b08e
ManashSwain <manash0802@gmail.com>
2025-03-18 20:18:24 +0530

    fixes in card component

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -7,45 +7,51 @@ const CourseCard = ({ course }) => {
   const { currency } = useContext(ContextProvider);
   return (
     <>
-    <Link to={"/course/" + course._id} onClick = {()=>{scrollTo(0,0)}}>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
-        
-        <a href="#">
+      <Link
+        to={"/course/" + course._id}
+        onClick={() => {
+          scrollTo(0, 0);
+        }}
+      >
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
           <img
             className="rounded-t-lg"
             src={course.courseThumbnail}
             alt="Thumbnail"
           />
-        </a>
-        <div className="p-5">
-          <a href="#">
+
+          <div className="p-5">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
               {course.courseTitle}
             </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 ">
-            {course.educator.name}
-          </p>
-          <div className="flex  gap-2 items-center">
-            <p>{4.5}</p>
-            <div className="flex">
-              {[...Array(5)].map((_, i) => (
-                <img key={i} src={assets.star} alt="star" className="w-4 h-4" />
-              ))}
-            </div>
 
-            <p className="text-gray-600">(22)</p>
-          </div>
-          <div className="font-bold">
-            {currency}
-            {(
-              course.coursePrice -
-              (course.discount * course.coursePrice) / 100
-            ).toFixed(2)}
+            <p className="mb-3 font-normal text-gray-700 ">
+              {course.educator.name}
+            </p>
+            <div className="flex  gap-2 items-center">
+              <p>{4.5}</p>
+              <div className="flex">
+                {[...Array(5)].map((_, i) => (
+                  <img
+                    key={i}
+                    src={assets.star}
+                    alt="star"
+                    className="w-4 h-4"
+                  />
+                ))}
+              </div>
+
+              <p className="text-gray-600">(22)</p>
+            </div>
+            <div className="font-bold">
+              {currency}
+              {(
+                course.coursePrice -
+                (course.discount * course.coursePrice) / 100
+              ).toFixed(2)}
+            </div>
           </div>
         </div>
-       
-      </div>
       </Link>
     </>
   );
