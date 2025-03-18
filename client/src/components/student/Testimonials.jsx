fea8b06d75dd7fc343dca8147470ec4b24095a6b
ManashSwain <manash0802@gmail.com>
2025-03-18 12:40:33 +0530

    changes in testimonial design

--- client/src/components/student/Testimonials.jsx
+++ client/src/components/student/Testimonials.jsx
@@ -8,6 +8,7 @@ const Testimonials = () => {
     <section className="text-gray-600 body-font bg-gray-100 py-12">
       <div className="container px-5 mx-auto">
         <h2 className="text-3xl font-bold text-center mb-6">Testimonials</h2>
+        <p className='mb-6 px-12 text-center'>Hear from our learners as they share their journeys of transformation, success, and how our platform has made a difference in their lives.</p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {dummyTestimonial.map((testimonial, index) => (
             <TestimonialCard key={index} {...testimonial} />
