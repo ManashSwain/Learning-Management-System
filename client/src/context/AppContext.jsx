edea8804b9cdffcff2cf06cbe10f73f647ac5c38
ManashSwain <manash0802@gmail.com>
2025-03-18 10:37:39 +0530

    added currency in context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,12 +4,9 @@ import React, { createContext } from 'react'
 export const ContextProvider = createContext();
 const AppContext = (props) => {
 
+  const currency = import.meta.env.VITE_CURRENCY;
 
-  const testfunction = ()=>{
-    console.log("Function tested")
-  }
-
-    const values = {testfunction}
+    const values = {currency}
   return (
     <>
     <ContextProvider.Provider value={values}>
