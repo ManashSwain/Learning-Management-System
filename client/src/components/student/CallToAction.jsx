bf1584e0dfad36cfd7dad594f4039b6e1390aa83
ManashSwain <manash0802@gmail.com>
2025-03-18 13:55:36 +0530

    refactored call to action ui design

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -5,20 +5,13 @@ const CallToAction = () => {
     <>
     <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-col text-center w-full mb-12">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
-      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep.</p>
+    <div className="flex flex-col text-center w-full mb-6">
+      <h1 className="sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900">Learn anything, anytime, anywhere</h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Join the best online learning platform and take your skills to the next level.</p>
     </div>
-    <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
-      <div className="relative flex-grow w-full">
-        <label for="full-name" className="leading-7 text-sm text-gray-600">Full Name</label>
-        <input type="text" id="full-name" name="full-name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <div className="relative flex-grow w-full">
-        <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+    <div className='flex gap-2 justify-center'>
+    <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get Started</button>
+      <button className="text-gray-500 bg-white border-[2px] border-gray-500 py-2 px-8 focus:outline-none  rounded text-lg ">Learn more</button>
     </div>
   </div>
 </section>
