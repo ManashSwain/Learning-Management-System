1c1d1ec855d6e7f28e5ca0406cae4d6372390473
ManashSwain <manash0802@gmail.com>
2025-03-18 12:20:36 +0530

    refactored testimonials ui design

---
+++ client/src/components/student/TestimonialCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const TestimonialCard = ({ name, role, image, feedback }) => {
+  return (
+  <>
+   <div className="bg-white shadow-lg rounded-lg p-6 text-center">
+      <img 
+        alt={name} 
+        className="w-20 h-20 mb-4 object-cover object-center rounded-full mx-auto border-2 border-gray-200 bg-gray-100" 
+        src={image} 
+      />
+      <p className="text-gray-600 leading-relaxed">{feedback}</p>
+      <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-4 mb-2"></span>
+      <h2 className="text-gray-900 font-semibold text-lg">{name}</h2>
+      <p className="text-gray-500 text-sm">{role}</p>
+    </div>
+
+  </>
+  )
+}
+
+export default TestimonialCard
