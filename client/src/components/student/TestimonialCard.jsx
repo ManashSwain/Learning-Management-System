a41f1002fc0d7186817b3865b27341d1e5e27914
ManashSwain <manash0802@gmail.com>
2025-03-18 22:37:16 +0530

    implemented dynamic star rating for testimonials section

--- client/src/components/student/TestimonialCard.jsx
+++ client/src/components/student/TestimonialCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
-const TestimonialCard = ({ name, role, image, feedback }) => {
+const TestimonialCard = ({ name, role, image, feedback ,rating}) => {
   return (
   <>
    <div className="bg-white shadow-lg rounded-lg p-6 text-center">
@@ -9,6 +10,13 @@ const TestimonialCard = ({ name, role, image, feedback }) => {
         className="w-20 h-20 mb-4 object-cover object-center rounded-full mx-auto border-2 border-gray-200 bg-gray-100" 
         src={image} 
       />
+      <div className='flex justify-center my-2'>
+        {
+          [...Array(5)].map((_,i)=>{
+            return<img  key={i} src={i < rating ? assets.star : assets.star_blank}/>
+          })
+        }
+      </div>
       <p className="text-gray-600 leading-relaxed">{feedback}</p>
       <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-4 mb-2"></span>
       <h2 className="text-gray-900 font-semibold text-lg">{name}</h2>
