903532671c3d9ad940c4df184047c1213796b9ec
ManashSwain <manash0802@gmail.com>
2025-03-18 11:26:39 +0530

    refactored search component ui design

--- client/src/components/student/Searchbar.jsx
+++ client/src/components/student/Searchbar.jsx
@@ -14,7 +14,7 @@ const Searchbar = () => {
   return (
     <>
     
-<form className="max-w-md mx-auto">   
+<form className="px-3 max-w-md mx-auto">   
     <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
     <div className="relative">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
