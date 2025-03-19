6bde05915656e1dcf040bfec9c13af17e5de4575
ManashSwain <manash0802@gmail.com>
2025-03-19 22:47:45 +0530

    loaded data in single course  page design

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ContextProvider } from '../../context/AppContext';
 
 const CourseDetails = () => {
+  const {id} = useParams();
+ 
+
+  const {courseData} = useContext(ContextProvider) ;
+  const [singleCourseData , getSingleCourseData] = useState(null);
+  
+  useEffect(()=>{
+    if(courseData && courseData.length > 0){
+      const singleCourse = courseData.find(course => course._id === id);
+      getSingleCourseData(singleCourse);
+    }
+  },[courseData , id])
+  if (!singleCourseData) {
+    return <p>Loading...</p>;
+  }
   return (
    <>
-   <h1>Course details</h1>
+   <div>
+    {/* left column  */}
+    <div>
+      <h1>{singleCourseData.courseTitle}</h1>
+    </div>
+    {/* right column  */}
+    <div></div>
+   </div>
+     
    </>
   )
 }
