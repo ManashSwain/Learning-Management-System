16ce01c5a5ce2e774567a327347274c43eb15f31
ManashSwain <manash0802@gmail.com>
2025-03-19 16:49:48 +0530

    ui changes in button

--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -79,7 +79,7 @@ const CourseList = () => {
       </div>
       {/* button  */}
       <div className="px-36 mt-6">{ input ?  
-      <button onClick={()=>{navigate('/course-list')}} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none ">{input}</button> : <></>}
+      <button onClick={()=>{navigate('/course-list')}} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none ">{input} <span className="pl-2 cursor-pointer">X</span> </button> : <></>}
       </div>
       {/* course list  */}
       <div className="px-36 mt-16 mb-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
